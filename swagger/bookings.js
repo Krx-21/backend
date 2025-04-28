fd256035aa32f8f3deca9d4b041eaeac3eca28d3
soqt <83688930+yusoqt@users.noreply.github.com>
2025-04-28 15:20:54 +0700

    Update bookings.js

--- swagger/bookings.js
+++ swagger/bookings.js
@@ -7,6 +7,60 @@
 
 /**
  * @swagger
+ * /cars/{carId}/bookings:
+ *   post:
+ *     summary: Create a new booking
+ *     tags: [Bookings]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: carId
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: Car ID
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - start_date
+ *               - end_date
+ *             properties:
+ *               start_date:
+ *                 type: string
+ *                 format: date-time
+ *                 description: Booking start date
+ *               end_date:
+ *                 type: string
+ *                 format: date-time
+ *                 description: Booking end date
+ *     responses:
+ *       201:
+ *         description: Booking created successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 success:
+ *                   type: boolean
+ *                   example: true
+ *                 data:
+ *                   $ref: '#/components/schemas/Booking'
+ *       400:
+ *         $ref: '#/components/responses/ValidationError'
+ *       401:
+ *         $ref: '#/components/responses/UnauthorizedError'
+ *       403:
+ *         $ref: '#/components/responses/ForbiddenError'
+ *       404:
+ *         $ref: '#/components/responses/NotFoundError'
+ *       500:
+ *         $ref: '#/components/responses/ServerError'
  * /bookings:
  *   get:
  *     summary: Get all bookings
@@ -57,61 +111,6 @@
  *         $ref: '#/components/responses/UnauthorizedError'
  *       500:
  *         $ref: '#/components/responses/ServerError'
- *   post:
- *     summary: Create a new booking
- *     tags: [Bookings]
- *     security:
- *       - bearerAuth: []
- *     requestBody:
- *       required: true
- *       content:
- *         application/json:
- *           schema:
- *             type: object
- *             required:
- *               - start_date
- *               - end_date
- *               - car
- *               - totalprice
- *             properties:
- *               start_date:
- *                 type: string
- *                 format: date-time
- *                 description: Booking start date
- *               end_date:
- *                 type: string
- *                 format: date-time
- *                 description: Booking end date
- *               car:
- *                 type: string
- *                 description: Car ID
- *               totalprice:
- *                 type: number
- *                 description: Total booking price
- *     responses:
- *       201:
- *         description: Booking created successfully
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 success:
- *                   type: boolean
- *                   example: true
- *                 data:
- *                   $ref: '#/components/schemas/Booking'
- *       400:
- *         $ref: '#/components/responses/ValidationError'
- *       401:
- *         $ref: '#/components/responses/UnauthorizedError'
- *       403:
- *         $ref: '#/components/responses/ForbiddenError'
- *       404:
- *         $ref: '#/components/responses/NotFoundError'
- *       500:
- *         $ref: '#/components/responses/ServerError'
- * 
  * /bookings/{id}:
  *   get:
  *     summary: Get a single booking
